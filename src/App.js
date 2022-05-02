0d6ea8e5087f87796034fcdcbbbb70cc979c3e45
Will Gundy <willgunderson503@gmail.com>
2022-05-02 14:54:09 -0700

    added slogan editing

--- src/App.js
+++ src/App.js
@@ -46,8 +46,9 @@ function App() {
           <SkylineDropdown setSkylineId={setSkylineId}/>
         </section>
         {/* here, the SloganForm component takes in the setSlogans state handler function and the slogans array that live in state */}
+        <SloganForm setCitySlogans={setCitySlogans} citySlogans={citySlogans}/>
         {/* here, the SloganList component takes the array of slogans that lives in state */}
-
+        <SloganList citySlogans={citySlogans}/>
       </div>
     </div>
   );
